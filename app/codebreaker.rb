64829f0d692c9a1503bc8f7c7d38f6b5dc7d6129
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-26 00:54:47 +0100

    Refactoring

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -17,18 +17,16 @@ class Codebreaker
     def guess(input)
       output.puts "Try guessing a number with four digits" unless input.length == 4
 
-      contains = false
-      input.each_char do |digit|
-        if @secret_number.include?(digit)
-          contains = true
-        end
-      end
 
+      contains = false
       exact_match = false
       input.chars.each_with_index do |input_value, input_index|
         @secret_number.chars.each_with_index do |secret_number_value, secret_number_index|
-          if input_value == secret_number_value && input_index == secret_number_index
-            exact_match = true
+          if input_value == secret_number_value
+            contains = true
+            if input_index == secret_number_index
+              exact_match = true
+            end
           end
         end
       end
