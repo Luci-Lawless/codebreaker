50bc90defc1f817d98b4b78b741da52d7a3d0ce4
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-25 23:12:28 +0100

    Show + for exact match

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -15,20 +15,32 @@ class Codebreaker
     end
 
     def guess(input)
-
       output.puts "Try guessing a number with four digits" unless input.length == 4
 
       contains = false
-
       input.each_char do |digit|
-        if (@secret_number.include?(digit))
+        if @secret_number.include?(digit)
           contains = true
         end
       end
 
-      output.puts '' unless contains
-      else
-        output.puts "-"
+      exact_match = false
+      input.chars.each_with_index do |input_value, input_index|
+        @secret_number.chars.each_with_index do |secret_number_value, secret_number_index|
+          if input_value == secret_number_value && input_index == secret_number_index
+            exact_match = true
+          end
+        end
       end
+
+      if exact_match
+        output.puts "+"
+      elsif contains
+        output.puts "-"
+      else
+        output.puts ''
+      end
+
+    end
   end
 end
