2535364d3ec409c18325b1586b4f3d01e4066e49
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-26 14:20:45 +0100

    Extract method for matched number that already exists

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -8,6 +8,7 @@ class Codebreaker
       @result = ''
       @pluses = ''
       @minuses = ''
+      @matched_numbers = []
     end
 
     def start(secret_number)
@@ -22,20 +23,18 @@ class Codebreaker
       validate_input_length(input)
 
 
-      matched_numbers = []
-
       input.chars.each_with_index do |input_value, input_index|
         @secret_number.chars.each_with_index do |secret_number_value, secret_number_index|
           if input_value == secret_number_value
             if input_index == secret_number_index
-              if !matched_numbers.include?(input_value)
+              if !match_exists(input_value)
                 @pluses += "+"
-                matched_numbers << input_value
+                @matched_numbers << input_value
               end
             else
-              if !matched_numbers.include?(input_value)
+              if !match_exists(input_value)
                 @minuses += "-"
-                matched_numbers << input_value
+                @matched_numbers << input_value
               end
             end
           end
@@ -48,6 +47,10 @@ class Codebreaker
 
     def validate_input_length(input)
       output.puts "Try guessing a number with four digits" unless input.length == MAX_INPUT_LENGTH
+    end
+
+    def match_exists(input_value)
+      return @matched_numbers.include?(input_value)
     end
 
     def display_result
