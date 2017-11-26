5cc95cfb26a1f0717da289bbf62c6627d35078e7
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-26 14:36:33 +0100

    Extract method to add number match and add exact match

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -27,15 +27,9 @@ class Codebreaker
         @secret_number.chars.each_with_index do |secret_number_value, secret_number_index|
           if input_value == secret_number_value
             if input_index == secret_number_index
-              if !match_exists(input_value)
-                @pluses += "+"
-                @matched_numbers << input_value
-              end
+              add_exact_match(input_value)
             else
-              if !match_exists(input_value)
-                @minuses += "-"
-                @matched_numbers << input_value
-              end
+              add_number_match(input_value)
             end
           end
         end
@@ -49,7 +43,21 @@ class Codebreaker
       output.puts "Try guessing a number with four digits" unless input.length == MAX_INPUT_LENGTH
     end
 
-    def match_exists(input_value)
+    def add_exact_match(input_value)
+      if !match_exists?(input_value)
+        @pluses += "+"
+        @matched_numbers << input_value
+      end
+    end
+
+    def add_number_match(input_value)
+      if !match_exists?(input_value)
+        @minuses += "-"
+        @matched_numbers << input_value
+      end
+    end
+
+    def match_exists?(input_value)
       return @matched_numbers.include?(input_value)
     end
 
